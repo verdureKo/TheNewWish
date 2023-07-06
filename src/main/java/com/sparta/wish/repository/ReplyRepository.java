2e336a53ddb498f3a6e65f9b4676cf34b54de1f6
yuio7279 <yuio7279@gmail.com>
2023-07-06 17:38:26 +0900

    일단 커밋

--- src/main/java/com/sparta/wish/repository/ReplyRepository.java
+++ src/main/java/com/sparta/wish/repository/ReplyRepository.java
@@ -2,6 +2,7 @@ package com.sparta.wish.repository;
 
 import com.sparta.wish.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
